6076066c480b58e2ff2d6f9417939f3c1e48af32
Dan Corin <dcorin6@gmail.com>
2024-08-19 21:06:11 -0400

    Add direnv function support

--- home/default.nix
+++ home/default.nix
@@ -112,6 +112,24 @@ in
       #   org.gradle.console=verbose
       #   org.gradle.daemon.idletimeout=3600000
       # '';
+
+      # https://github.com/direnv/direnv/issues/73#issuecomment-392342423
+      ".direnvrc".text = ''
+      export_function() {
+        local name=$1
+        local alias_dir=$PWD/.direnv/aliases
+        mkdir -p "$alias_dir"
+        PATH_add "$alias_dir"
+        local target="$alias_dir/$name"
+        if declare -f "$name" >/dev/null; then
+          echo "#!$SHELL" > "$target"
+          declare -f "$name" >> "$target" 2>/dev/null
+          # Notice that we add shell variables to the function trigger.
+          echo "$name \$*" >> "$target"
+          chmod +x "$target"
+        fi
+      }
+      '';
     };
   };
 
